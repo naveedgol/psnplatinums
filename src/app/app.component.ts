d86a5a93304c77a1365da1304bd397ca198a38e1
Naveed Golafshani <ngolafshani@live.ca>
2020-10-31 15:34:01 -0400

    levelling icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -71,6 +71,33 @@ export class AppComponent {
   }
 
   getLevelImage(): string {
+    if (this.user.level < 100) {
+      return "assets/images/bronze.png";
+    }
+    if (this.user.level < 200) {
+      return "assets/images/bronze2.png";
+    }
+    if (this.user.level < 300) {
+      return "assets/images/bronze3.png";
+    }
+    if (this.user.level < 400) {
+      return "assets/images/silver.png";
+    }
+    if (this.user.level < 500) {
+      return "assets/images/silver2.png";
+    }
+    if (this.user.level < 600) {
+      return "assets/images/silver3.png";
+    }
+    if (this.user.level < 700) {
+      return "assets/images/gold.png";
+    }
+    if (this.user.level < 800) {
+      return "assets/images/gold2.png";
+    }
+    if (this.user.level < 999) {
+      return "assets/images/gold3.png";
+    }
     return "assets/images/platinum.png";
   }
 
