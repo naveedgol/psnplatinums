5da59b9ad3b7217d3073c22d719b0ff41785fb3e
Naveed Golafshani <ngolafshani@live.ca>
2021-01-18 02:51:02 -0500

    Share to Reddit button

--- src/app/components/trophy-case/trophy-case.component.ts
+++ src/app/components/trophy-case/trophy-case.component.ts
@@ -4,6 +4,7 @@ import { Trophy } from '../../types/Trophy';
 import { DisplaySettings } from '../../types/DisplaySettings';
 import html2canvas from 'html2canvas';
 import { from } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-trophy-case',
@@ -16,6 +17,9 @@ export class TrophyCaseComponent {
   @Input() user: User;
   @Input() displaySettings: DisplaySettings;
   @Output() uponIsSaveLoading: EventEmitter<any> = new EventEmitter(false);
+
+  constructor(private http: HttpClient) {
+  }
 
   getLevelImage(): string {
     if (this.user.level < 100) {
@@ -72,5 +76,35 @@ export class TrophyCaseComponent {
         this.uponIsSaveLoading.emit(false);
       }
     )
+  }
+
+  public share(): void {
+    this.uponIsSaveLoading.emit(true);
+    const clientId = "eec09f734fad776";
+
+    let element = document.querySelector("#capture");
+    from(html2canvas(element as HTMLElement, { useCORS: true, scrollX: 0, scrollY: -window.scrollY })).subscribe(
+      canvas => {
+        (canvas as HTMLCanvasElement).toBlob((blob) => {
+          var data = new FormData();
+          data.append("image", blob);
+
+          const options = {
+            responseType: 'json' as const,
+            headers: { "Authorization": "Client-ID " + clientId },
+          };
+          this.http.post("https://api.imgur.com/3/image", data, options).subscribe(
+            data => {
+              const imgurLink = data["data"]["link"];
+              const redditLink = "http://www.reddit.com/submit?url=" + imgurLink + "&title=%5BMultiple+Games%5D+My+Platinum+mosaic+built+with+psnplatinums.com";
+              window.open(redditLink, "_blank");
+              this.uponIsSaveLoading.emit(false);
+            }
+          )
+        }, 'image/png');
+      }
+    )
+
+
   }
 }
